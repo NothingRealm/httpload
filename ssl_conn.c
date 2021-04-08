421850cee5da6f5b5cee07c8786667b076a4139d
shayandesh <shafaghi1378@gmail.com>
2021-04-08 15:26:58 +0430

    Creating SSL connection

--- ssl_conn.c
+++ ssl_conn.c
@@ -9,3 +9,23 @@ void init_ssl() {
 }
 
 
+void create_ssl_connection(SSL *ssl, SOCKET *sock) {
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        ERROR("Could not create SSL context!!!");
+        return ERR_SSL_CTX;
+    }
+
+    SSL *ssl = SSL_new(ctx);
+    if (!ssl) {
+        ERROR("Could not SSL!!!");
+        return ERR_SSL;
+    }
+
+    SSL_set_fd(ssl, sock);
+    if (SSL_connect(ssl) == -1) {
+        ERROR("Could not make SSL Connection!!!");
+        return ERR_SSL_CONN;
+    }
+    return SUCCESS;
+}
--- ssl_conn.h
+++ ssl_conn.h
@@ -1,4 +1,13 @@
 #ifndef SSL_CONN_H
 #define SSL_CONN_H
 
+#define SOCKET int
+
+#define SUCCESS 0
+#define ERR_SSL_CTX -1
+#define ERR_SSL -2
+#define ERR_SSL_CONN -3
+
+#include <openssl/ssl.h>
+
 #endif
