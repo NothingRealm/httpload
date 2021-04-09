1034fb986b923fca1a3ee9b7cdb1fa18370126d7
shayandesh <shafaghi1378@gmail.com>
2021-04-09 17:26:37 +0430

    Added some error constants and function prototype

--- ev_epoll.h
+++ ev_epoll.h
@@ -2,10 +2,17 @@
 #define EV_EPOLL_H
 
 #include "ev.h"
+#include "ssl_conn.h"
+#include <sys/epoll.h>
 
 #define READ_EVENT (EPOLLIN|EPOLLRDHUP)
 #define WRITE_EVENT EPOLLOUT
 #define CLEAR_EVENT EPOLLET
 
+#define ERR_EPOLL_CREATE -6
+#define ERR_EPOLL_WAIT -7
+
+int add_new_connection(SSL *ssl, int epollfd);
+int wait_for_event(int epollfd);
 
 #endif
