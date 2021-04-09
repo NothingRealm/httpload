747201c731c1252de9e773da41227ecf4bdadc14
shayandesh <shafaghi1378@gmail.com>
2021-04-09 20:09:58 +0430

    Added domain resolver

--- ssl_conn.c
+++ ssl_conn.c
@@ -33,25 +33,37 @@ int create_ssl_connection(SSL *ssl, SOCKET *sock) {
 }
 
 
-int socket_connect_to_host(char* hostname, char* port, SOCKET *sock) {
-    struct addrinfo hints;
-    struct addrinfo *peer_address;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(hostname, port, &hints, &peer_address);
-
-    *sock = socket(peer_address->ai_family, peer_address->ai_socktype,
-            peer_address->ai_protocol);
+int socket_connect_to_host(struct sockaddr_in* peer_address, SOCKET *sock) {
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
     if (*sock == -1) {
         ERROR("socket() failed!!!");
         return ERR_SOCKET_CREATE;
     }
-    if (connect(*sock, peer_address->ai_addr, peer_address->ai_addrlen)) {
+    if (connect(*sock, (struct sockaddr*)peer_address, sizeof(*peer_address))) {
         ERROR("connect() failed!!!");
         return ERR_SOCKET_CONNECT;
     }
-    freeaddrinfo(peer_address);
     return SUCCESS;
 }
 
+
+int domain_lookup(char* hostname, char* port, struct sockaddr_in* addr_in) {
+    int err;
+    struct addrinfo hints, *peer_address;
+    
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;
+
+    err = getaddrinfo(hostname, port, &hints, &peer_address);
+
+    if (!err)
+        return err;
+
+    addr_in = (struct sockaddr_in*) peer_address->ai_addr;
+
+    printf("%s:%d\n", inet_ntoa(addr_in->sin_addr), ntohs(addr_in->sin_port));
+
+    freeaddrinfo(peer_address);
+
+    return SUCCESS;
+}
