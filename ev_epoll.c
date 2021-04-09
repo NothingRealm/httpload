4751fae39e17467a2826014017edfb5a010af9d2
shayandesh <shafaghi1378@gmail.com>
2021-04-10 01:12:53 +0430

    Simple epoll for read and write

--- ev_epoll.c
+++ ev_epoll.c
@@ -2,18 +2,25 @@
 
 int loop() {
     int err, epollfd, nfds, n, bytes_recieved;
+    struct sockaddr_in peer_address;
     struct epoll_event ev, events[MAX_EVENTS];
     char buffer[BUFFER_SIZE];
+    char* host = "localhost";
+    char* port = "8000";
     SOCKET sock; 
     SSL *ssl;
 
     init_ssl();
 
-    err = socket_connect_to_host("localhost", "8000", &sock);
+    domain_lookup(host, port, &peer_address);
+    printf("%s:%d\n", inet_ntoa(peer_address.sin_addr), ntohs(peer_address.sin_port));
+
+    err = socket_connect_to_host(&peer_address, &sock);
+
     if (err < 0)
         exit(err);
 
-    err = create_ssl_connection(ssl, &sock);
+    ///err = create_ssl_connection(ssl, &sock);
     if (err < 0)
         exit(err);
 
@@ -24,29 +31,36 @@ int loop() {
     }
 
     ev.events = READ_EVENT | WRITE_EVENT;
-    ev.data.ptr = ssl;
+    ev.data.ptr = &sock;
 
     epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev);
 
 
     while (1) {
         nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-        if (nfds < 0) {
+        if (nfds == -1) {
             ERROR("epoll_wait() failed");
             exit(ERR_EPOLL_WAIT);
         }
+        printf("%d\n", nfds);
         for (n = 0; n < nfds; n++) {
-            printf("here\n");
-            if (events[n].events || WRITE_EVENT) {
-                sprintf(buffer, "Hello There");
-                SSL_write((SSL *) events[n].data.ptr, buffer, strlen(buffer));
-            }
+            sock = *(SOCKET*) events[n].data.ptr;
             if (events[n].events || READ_EVENT) {
-                bytes_recieved = SSL_read((SSL *) events[n].data.ptr, buffer, BUFFER_SIZE);
+                //bytes_recieved = SSL_read((SSL *) events[n].data.ptr, buffer, BUFFER_SIZE);
+                printf("reading\n");
+                read(sock, buffer, BUFFER_SIZE);
                 if (bytes_recieved < 0)
-                    printf();
+                    continue;
+                printf("reading finished\n");
                 printf("%s\n", buffer);
+            }
+            if (events[n].events || WRITE_EVENT) {
+                printf("writing\n");
+                sprintf(buffer, "Hello There");
+                write(sock, buffer, BUFFER_SIZE);
+                //SSL_write((SSL *) events[n].data.ptr, buffer, strlen(buffer));
             }
         }
     }
+
 }
--- ev_epoll.h
+++ ev_epoll.h
@@ -5,12 +5,12 @@
 #include "ssl_conn.h"
 #include <sys/epoll.h>
 
-#define READ_EVENT (EPOLLIN|EPOLLRDHUP)
+#define READ_EVENT (EPOLLIN | EPOLLRDHUP)
 #define WRITE_EVENT EPOLLOUT
 #define CLEAR_EVENT EPOLLET
 
-#define ERR_EPOLL_CREATE -6
-#define ERR_EPOLL_WAIT -7
+#define ERR_EPOLL_CREATE -7
+#define ERR_EPOLL_WAIT -8
 
 int add_new_connection(SSL *ssl, int epollfd);
 int wait_for_event(int epollfd);
