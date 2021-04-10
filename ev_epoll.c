dece27a7873b9166a3506420143fea5216927e36
shayandesh <shafaghi1378@gmail.com>
2021-04-10 21:26:07 +0430

    Did a little seperation of concepts

--- ev_epoll.c
+++ ev_epoll.c
@@ -1,5 +1,31 @@
 #include "ev_epoll.h"
 
+
+int ev_init(int* epollfd) {
+    *epollfd = epoll_create1(0);
+    if(epollfd < 0) {
+        ERROR("epoll_create1() failed");
+        return ERR_EPOLL_CREATE;
+    }
+}
+
+
+int ev_add(int epollfd, SSL* ssl, int sock) {
+    int err;
+    struct epoll_event ev;
+    ev.events = WRITE_EVENT | READ_EVENT;
+    ev.data.ptr = ssl;
+    err = epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev);
+    if (err == -1)
+        return ERR_EPOLL_CTL;
+    return SUCCESS;
+}
+
+int ev_wait(int epollfd, struct epoll_event *events) {
+    return epoll_wait(epollfd, events, MAX_EVENTS, -1);
+}
+
+
 int loop() {
     int err, epollfd, nfds, n, bytes_recieved, count;
     struct sockaddr_in peer_address;
@@ -7,16 +33,14 @@ int loop() {
     char buffer[BUFFER_SIZE];
     char* host = "google.com";
     char* port = "443";
-    SOCKET sock, peer_sock; 
+    SOCKET sock; 
     SSL *ssl;
 
     domain_lookup(host, port, &peer_address);
 
     init_ssl();
 
-
     err = socket_connect_to_host(&peer_address, &sock);
-
     if (err < 0)
         exit(err);
 
@@ -24,19 +48,16 @@ int loop() {
     if (err < 0)
         exit(err);
 
-    epollfd = epoll_create1(0);
-    if(epollfd < 0) {
-        ERROR("epoll_create1() failed");
-        exit(ERR_EPOLL_CREATE);
-    }
+    err = ev_init(&epollfd);
+    if (err < 0)
+        exit(err);
 
-    ev.events = WRITE_EVENT | READ_EVENT;
-    ev.data.ptr = ssl;
-
-    epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev);
+    err = ev_add(epollfd, &ssl, sock);
+    if (err < 0)
+        exit(err);
 
     while (1) {
-        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+        nfds = ev_wait(epollfd, events);
         if (nfds == -1) {
             ERROR("epoll_wait() failed");
             exit(ERR_EPOLL_WAIT);
@@ -48,7 +69,8 @@ int loop() {
                 printf("writing\n");
             }
             if (events[n].events & READ_EVENT) {
-                bytes_recieved = SSL_read((SSL *) events[n].data.ptr, buffer, BUFFER_SIZE);
+                bytes_recieved = SSL_read((SSL *) events[n].data.ptr, buffer,
+                        BUFFER_SIZE);
                 if (bytes_recieved < 0)
                     continue;
                 printf("Read: %s\n", buffer);
